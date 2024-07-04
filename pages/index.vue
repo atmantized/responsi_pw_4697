a4597038e100b925836d1bd0560ad5ba531f9b21
atmantized <eleonoraiinez@gmail.com>
2024-07-04 15:13:20 +0700

    responsi belum

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,7 @@ import HeroSection from '~/components/HeroSection.vue';
 import AboutSection from '~/components/AboutSection.vue';
 import ProjectsSection from '~/components/ProjectsSection.vue';
 import ContactSection from '~/components/ContactSection.vue';
+// import ContactSection from '~/components/ContactSection.vue';
 
 export default {
   components: {
